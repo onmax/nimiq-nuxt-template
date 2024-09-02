b02984e764bb2b9c9a1885978c3de3558d9577cd
Max <maximogarciamtnez@gmail.com>
2024-09-02 16:31:51 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,6 +59,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      title: 'Nimiq Nuxt Template',
       viewport: 'width=device-width,initial-scale=1',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
